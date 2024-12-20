7b753d44462451696e7f22a4b12b516985ffc571
Dawn Cheairs <dawnsusanne@gmail.com>
2024-12-19 19:14:33 -0700

    Styled the elements, added a form

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 console.log('is this thing on?')
 
+function displayGreeting(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value; // get the user's name
+    document.getElementById('greeting').innerHTML = `Happy knitting, ${name}`; 
+    // document.querySelector("form").reset();
+}
+let letsGo = document.querySelector("form")
+console.log(letsGo)
+console.log(letsGo.name)
+
 let count = 0
 let countEl = document.getElementById("count-el")
 console.log(countEl)
 
 function increment() {
     count = count + 1
-    countEl.innerText = count
+    countEl.textContent = count
 }
-console.log(count)
 
 
 let saveEl = document.getElementById("save-el")
@@ -16,16 +25,22 @@ console.log(saveEl)
 
 function save(){
     let countStr = count + " - "
-    saveEl.innerText += countStr
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
     console.log(count)
 }
+
+// add in a button to change the colors of the count
 
 "reset-btn"
 let resetEl = document.getElementById("reset-el")
 console.log(resetEl)
+// confirm('Are you sure?')
 
 function reset(){
     let countStr = 0
-    saveEl.innerText = 0
+    saveEl.textContent = 0
     console.log(count)
+    alert('you have reset your counter')
 }
